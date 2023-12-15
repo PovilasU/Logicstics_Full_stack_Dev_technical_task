9934d7b6de4bdca78197e18099e75fc78c24d3cb
PovilasU <povilas.urbonas85@gmail.com>
2023-12-15 11:51:15 +0000

    Implement SearchBox component for driver search functionality

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const SearchBox = ({ onSearch }) => {
+  cost[(searchTerm, setSearchTerm)] = useState("");
+  const handleSearch = () => {
+    onSearch(searchTerm);
+  };
+
+  return (
+    <div className="search-box">
+      <input
+        type="text"
+        placeholder="Search drivers..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <button onClick={handleSearch}>Search</button>
+    </div>
+  );
+};
+
+export default SearchBox;
